7cec67deb36c8d2ab6001ca6b39a903d8208eb22
guan <guantemp@126.com>
2020-05-28 22:00:30 +0800

    202005282200

--- core/src/main/java/catalog/hoprxi/core/domain/model/barcode/UPC_A.java
+++ core/src/main/java/catalog/hoprxi/core/domain/model/barcode/UPC_A.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019. www.hoprxi.com All Rights Reserved.
+ * Copyright (c) 2020. www.hoprxi.com All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -40,6 +40,31 @@ public class UPC_A extends Barcode {
     }
 
     public UPC_E toUPC_E() {
-        return null;
+        StringBuffer result = new StringBuffer();
+        result.append(barcode.charAt(0));
+        if (barcode.charAt(0) != '0' && barcode.charAt(0) != '1') {
+            throw new InvalidBarcodeException("[UPCA] Invalid Number System,  only 0 & 1 are valid (" + barcode.charAt(0) + ").");
+        } else if ("000".equals(barcode.subSequence(3, 6)) || "100".equals(barcode.subSequence(3, 6)) || "200".equals(barcode.subSequence(3, 6))) {
+            result.append(barcode.subSequence(1, 3));
+            result.append(barcode.subSequence(8, 11));
+            result.append(barcode.charAt(3));
+        } else if ("00".equals(barcode.subSequence(4, 6))) {
+            result.append(barcode.subSequence(1, 4));
+            result.append(barcode.subSequence(9, 11));
+            result.append('3');
+        } else if (barcode.charAt(5) == '0') {
+            result.append(barcode.subSequence(1, 5));
+            result.append(barcode.charAt(10));
+            result.append('4');
+        } else if (barcode.charAt(10) >= '5' && barcode.charAt(10) <= '9') {
+            result.append(barcode.subSequence(1, 6));
+            result.append(barcode.charAt(10));
+        } else {
+            throw new InvalidBarcodeException("[UPCA] Invalid code.");
+        }
+        if (barcode.length() == 12) {
+            result.append(barcode.charAt(11));
+        }
+        return new UPC_E(result.toString());
     }
 }
